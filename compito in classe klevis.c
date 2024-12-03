1e4cb27b98376c981b0d0e056f4e2aeaf67f5447
klevisceraj <klevis.ceraj@davincimilazzo.edu.it>
2024-12-03 13:28:04 +0100

    Add files via upload

---
+++ compito in classe klevis.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	printf("somma");
+	printf("differenza");
+	printf("moltiplicazione");
+	printf("divisione");
+	int somma (int num1, int num2);
+	int differenza (int num1, int num2);
+	int moltiplicazione (int num1, int num2);
+	float divisione (float num1, float num2);
+}
+int somma (int num1,int num2){
+	int num1 ,int num2 ,tot=0
+	printf("inserisci un numero:\n");
+	scanf("%d",&num1);
+	printf("inserisci un numero:\n");
+	scanf("%d",&num2);
+	tot=num1+num2
+	printf("%d+%d= %d"num1, num2, tot somma);
+}
+int differenza (int num1, int num2);{
+	int diff=0;
+	printf("inserisci un numero:\n");
+	scanf("%d",&num1);
+	printf("inserisci un numero:\n");
+	scanf("%d",&num2);
+	diff=num1+num2;
+	printf("%d+%d= %d"num1, num2, diff);
+}
+int moltiplicazione (int num1, int num2);{
+	int moltip=0;
+	printf("inserisci un numero:\n");
+	scanf("%d",&num1);
+	printf("inserisci un numero:\n");
+	scanf("%d",&num2);
+	diff=num1+num2;
+	printf("%d+%d= %d"num1, num2, moltip);
+}
+float divisione (float num1, float num2);{
+	int div=0;
+	printf("inserisci un numero:\n");
+	scanf("%d",&num1);
+	printf("inserisci un numero:\n");
+	scanf("%d",&num2);
+	diff=num1+num2;
+	printf("%d+%d= %d"num1, num2, div);
+}
+    
+    
